4539bc6b2d5bdce24a5671919dcdb5de9762e9db
KeeratiNon <keeratinon3@gmail.com>
2024-07-05 13:48:02 +0700

    fix: syntax in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import logoFooter from "../assets/svgs/logo-footer.svg"
 
 const Footer = () => {
   return (
-    <div className="bg-black py-[80px] flex justify-center items-center">
+    <footer className="bg-black py-[80px] flex justify-center items-center">
       <div className="flex flex-col items-center gap-[24px]">
         <img src={logoFooter} alt="logo" />
         <h1 className="text-white text-[24px] leading-[32px] font-bold ">Find your perfect pet sitter with us.</h1>
       </div>
-    </div>
+    </footer>
   );
 };
 
