85383ed120e17e663a0715638062dae48eb22fe3
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-07-07 17:39:32 +0700

    feat: add main section

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
-import Footer from "../components/Footer";
 import Navbar from "../components/navbar/Navbar";
+import MainContent from "../components/MainContent";
+import Linktologin from "../components/Linktologin";
+import Footer from "../components/Footer";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
+      <MainContent />
+      <Linktologin />
       <Footer />
     </>
   );
