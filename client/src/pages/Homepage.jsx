14489ed31453a9f185eb4b9cb5820d7fa055a568
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-07-08 00:10:30 +0700

    feat: add header

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/navbar/Navbar";
+import Header from "../components/Header";
 import MainContent from "../components/MainContent";
 import Linktologin from "../components/Linktologin";
 import Footer from "../components/Footer";
@@ -7,6 +8,7 @@ const Homepage = () => {
   return (
     <>
       <Navbar />
+      <Header />
       <MainContent />
       <Linktologin />
       <Footer />
