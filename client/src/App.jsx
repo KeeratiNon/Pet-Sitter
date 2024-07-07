212a6644b57952989f3f6cb511ef959700109944
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-07-07 20:32:59 +0700

    fix: resolve style linktologin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      {/* <LoginPage /> */}
       <Homepage />
     </>
   );
