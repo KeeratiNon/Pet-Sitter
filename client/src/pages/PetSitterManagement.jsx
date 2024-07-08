554c8d72069e9db8699ecd3a39861d436829ffb4
Surakit <surakitkunkayun@gmail.com>
2024-07-08 22:20:19 +0700

    feat: install packages

---
+++ client/src/pages/PetSitterManagement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/pet-sitter-management/PetSitter-NavBar";
+import PetsitterProfileForm from "../components/pet-sitter-management/PetSitterProfileForm";
+
+const PetSitterManagement = () => {
+    return (
+        <>
+        <PetsitterProfileForm/>
+        </>
+    )
+}
