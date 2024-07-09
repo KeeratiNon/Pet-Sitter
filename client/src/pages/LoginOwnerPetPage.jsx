115734d246f4268b3442dfe347936a3d083a8a38
KeeratiNon <keeratinon3@gmail.com>
2024-07-09 22:59:45 +0700

    fix: delete some code

--- client/src/pages/LoginOwnerPetPage.jsx
+++ client/src/pages/LoginOwnerPetPage.jsx
@@ -109,18 +109,6 @@ const LoginOwnerPetPage = () => {
                 <div className="w-[100%] flex justify-center items-center gap-[12px]">
                   <LoginFacebook />
                   <LoginGoogle />
-                  {/* <button className="btn-social">
-                    <span>
-                      <img src={FacebookLogo} alt="Facebook" />
-                    </span>
-                    <span>Facebook</span>
-                  </button>
-                  <button className="btn-social">
-                    <span>
-                      <img src={GoogleLogo} alt="Google" />
-                    </span>
-                    <span>Google</span>
-                  </button> */}
                 </div>
                 <div className="flex justify-center items-center gap-[8px]">
                   <span>
--- server/app.mjs
+++ server/app.mjs
@@ -1,5 +1,4 @@
 import express from "express"
-import dotenv from "dotenv"
 import { authRouter } from "./routes/auth.mjs";
 import cors from "cors"
 
