b53e67b72168e79d491ef67681a73639428e9d5e
KeeratiNon <keeratinon3@gmail.com>
2024-07-21 13:22:20 +0700

    feat: socket context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { AuthProvider } from "./contexts/authentication.jsx";
 import { BrowserRouter } from "react-router-dom";
 import jwtInterceptor from "./utils/jwtinterceptor.mjs";
+import { SocketProvider } from "./contexts/socket.jsx";
 
 jwtInterceptor();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
+      <SocketProvider>
         <App />
+      </SocketProvider>
     </AuthProvider>
   </BrowserRouter>
 );
