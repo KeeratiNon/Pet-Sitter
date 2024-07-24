2e1ca40aedee434afd113a6dd937ad7e320f04b3
KeeratiNon <keeratinon3@gmail.com>
2024-07-24 09:31:59 +0700

    feat: mock up upload image in chat

--- client/src/components/chat/FooterPetSitter.jsx
+++ client/src/components/chat/FooterPetSitter.jsx
@@ -8,18 +8,70 @@ const FooterPetSitter = ({
   setNewMessage,
   chatRoomId,
 }) => {
+  const [images, setImages] = useState([]);
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       sendMessage(chatRoomId);
     }
   };
 
+  const onImageChange = (e) => {
+    const selectedFiles = e.target.files;
+    if (selectedFiles.length > 0) {
+      const newImages = [...images];
+      for (let i = 0; i < selectedFiles.length; i++) {
+          newImages.push(selectedFiles[i]);
+      }
+      setImages(newImages);
+    }
+  };
+
+  const removeImage = (index) => {
+    const updatedImages = [...images];
+    updatedImages.splice(index, 1);
+    setImages(updatedImages);
+  };
+
+  const clearImages = () => {
+    setImages([]);
+  };
+
   return (
     <div className="flex justify-between gap-[24px] items-center">
-      <button className="bg-gray-100 p-[14px] rounded-full">
-        <img src={UploadImg} alt="" width="20.85px" height="18px" />
-      </button>
-      <span className=" py-[10px] flex flex-1">
+      <label
+        htmlFor="file-upload"
+        className="bg-gray-100 p-[14px] rounded-full cursor-pointer"
+      >
+        <img src={UploadImg} alt="uploadImage" width="20.85px" height="18px" />
+      </label>
+      <input
+        id="file-upload"
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={onImageChange}
+        multiple
+      />
+      <span className=" py-[10px] flex flex-1 gap-4">
+      {images.length > 0 &&
+          images.map((image, index) => (
+            <div key={index} className="relative">
+              <img
+                src={URL.createObjectURL(image)}
+                alt={`image-${index}`}
+                width="100px"
+                height="100px"
+                className="rounded"
+              />
+              <button
+                className="absolute top-[-10px] right-[-10px] bg-red-500 text-white text-[10px] rounded-full w-[20px] h-[20px]"
+                onClick={() => removeImage(index)}
+              >
+                X
+              </button>
+            </div>
+          ))}
         <input
           type="text"
           placeholder="Message here..."
@@ -33,6 +85,7 @@ const FooterPetSitter = ({
         className="p-[12px] bg-orange-500 rounded-full"
         onClick={() => {
           sendMessage(chatRoomId);
+          clearImages()
         }}
       >
         <img src={PostMsg} alt="close" />
