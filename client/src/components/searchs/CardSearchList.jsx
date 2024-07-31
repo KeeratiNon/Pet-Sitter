ee4664f50155d8458e8c7cb83dcf5cb4e62b0bcb
fossi13 <fossi13@users.noreply.github.com>
2024-07-31 09:20:54 +0700

    fix: add array [0]

--- client/src/components/searchs/CardSearchList.jsx
+++ client/src/components/searchs/CardSearchList.jsx
@@ -11,12 +11,12 @@ const CardSearchList = ({ profiles }) => {
       <div className=" w-full   rounded-[16px] p-4 gap-4 flex flex-col  xl:flex-row md:gap-6  md:bg-white  ">
         <div className="  md:w-[245px] md:h-[184px]  ">
           <img
-            src={profiles.image_gallery}
+            src={profiles.image_gallery[0]}
             alt="profild-web"
             className="hidden  rounded-lg md:w-[245px] md:h-[184px] md:block   "
           />
           <img
-            src={profiles.image_gallery}
+            src={profiles.image_gallery[0]}
             alt="card-img"
             className=" w-full  md:min-w-full md:min-h-[184px] rounded-[8px] md:hidden "
           />
